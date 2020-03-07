771dfb0a082ca72bd5c705b3069d3aaf3f2d0006
mo-sheikh <mohamed.sheikh@bbc.co.uk>
2020-03-07 20:05:34 +0000

    implemented requestStream endpoint

--- DAZN/src/server.js
+++ DAZN/src/server.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const axios = require("axios");
 const app = express();
-const data = require("../../DummyData/dummyData.json");
+const bodyParser = require("body-parser");
+const dummyFile = "../../DummyData/dummyData.json";
+const data = require(dummyFile);
+fs = require("fs");
 
 let test = () => {
   return true;
 };
+
+app.use(
+  bodyParser.text({
+    type: "*/*"
+  })
+);
 
 app.get("/", (req, res) => {
   res.send("OK!");
@@ -13,6 +22,18 @@ app.get("/", (req, res) => {
 
 app.get("/getUser", (req, res) => {
   //auth?
+  try {
+    let user = fetchUser(req.query.id);
+    res.send(user);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+//for unit testing purposes
+app.post("/createUser", (req, res) => {
+  //auth?
+  //get the body and append it to our json
   console.log("id is ", req.query.id);
   let UserId = req.query.id;
 
@@ -20,11 +41,49 @@ app.get("/getUser", (req, res) => {
 });
 
 app.get("/getAllUsers", (req, res) => {
-  //auth
+  //auth?
   console.log("id is ", req.query.id);
-  let UserId = req.query.id;
   res.send(data);
 });
+
+app.get("/requestStream", (req, res) => {
+  //fix this
+  let UserId = req.query.id;
+  let stream = req.query.stream;
+  try {
+    let user = fetchUser(UserId);
+    let activeStreams = user.Status.Online.Streams.activeStreams;
+    let currentStreams = user.Status.Online.Streams.watching;
+
+    switch (true) {
+      case currentStreams.includes(stream):
+        res.send(`You are already watching ${stream}`);
+        break;
+      case activeStreams >= 3:
+        res.send(
+          `User - ${user.Name} has exceeded maximum concurrent streams allowed.`
+        );
+        break;
+      default:
+        activeStreams += 1;
+        data.Users[UserId].Status.Online.Streams.activeStreams = activeStreams;
+        data.Users[UserId].Status.Online.Streams.watching.push(
+          stream.toLowerCase()
+        );
+        fs.writeFile("DummyData/dummyData.json", JSON.stringify(data), err => {
+          console.error("Can't update file", err);
+        });
+        res.send(`Request for ${stream} succesful`);
+    }
+  } catch (err) {
+    console.error(err);
+  }
+});
+let fetchUser = id => {
+  console.log("id is ", id);
+  let UserId = id;
+  return data.Users[UserId];
+};
 
 /*
 1) get user
--- Tests/Server.spec.js
+++ Tests/Server.spec.js
@@ -13,3 +13,48 @@ describe("Testing the root path", () => {
       });
   });
 });
+
+describe("Testing the we can create a dummy user", () => {
+  test("We should have a status of 200", done => {
+    request(DAZN.app)
+      .post("/createUser")
+      .then(response => {
+        expect(response.statusCode).toBe(200);
+        expect(response.text).toBe("OK!");
+        console.log("RESPONSE IS ", response.text);
+        done();
+      });
+  });
+});
+
+describe("Testing the we can fetch our dummy user", () => {
+  test("We should have a status of 200", done => {
+    request(DAZN.app)
+      .get("/")
+      .then(response => {
+        expect(response.statusCode).toBe(200);
+        expect(response.text).toBe("OK!");
+        console.log("RESPONSE IS ", response.text);
+        done();
+      });
+  });
+});
+
+describe("Testing that users cannot watch more than 3 streams", () => {
+  test("We should have a status of 200", done => {
+    request(DAZN.app)
+      .get("/")
+      .then(response => {
+        expect(response.statusCode).toBe(200);
+        expect(response.text).toBe("OK!");
+        console.log("RESPONSE IS ", response.text);
+        done();
+      });
+  });
+});
+
+//get stream that doesnt exist.
+
+//get user that doesnt exist
+
+//have the ability to create a fake user
